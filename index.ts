172a35287237df277753c004e59dd1caaf78b8dc
Ruan Martinelli <martinelliruan@gmail.com>
2023-12-04 20:15:05 +0000

    +

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import { getInput, info } from "@actions/core";
+import { getInput, info,debug } from "@actions/core";
 
 export async function main() {
   const token = getInput("frevo_token", { required: true });
   const path = getInput("path", { required: true });
   const config = getInput("config", { required: false });
 
-  info(
+  debug(
     JSON.stringify({
       token,
       path,
