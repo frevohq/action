e39e8826e53870cb5ac3371c1018c4dd82e45e0e
Ruan Martinelli <martinelliruan@gmail.com>
2023-12-05 05:47:40 +0000

    +

--- dist/index.js
+++ dist/index.js
@@ -8,19 +8,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = void 0;
 const core_1 = require("@actions/core");
+const oas_normalize_1 = __importDefault(require("oas-normalize"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const token = (0, core_1.getInput)("frevo_token", { required: true });
         const path = (0, core_1.getInput)("path", { required: true });
         const config = (0, core_1.getInput)("config", { required: false });
-        (0, core_1.info)(JSON.stringify({
-            token,
-            path,
-            config,
-        }));
+        const oas = new oas_normalize_1.default(path, { enablePaths: true });
+        yield oas.load();
+        (0, core_1.info)("OAS Specification loaded. Version: " + (yield oas.version()).version);
         return;
     });
 }
--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import { getInput, info } from "@actions/core";
+import OASNormalize from "oas-normalize";
+import OAS from "oas";
 
 export async function main() {
   const token = getInput("frevo_token", { required: true });
   const path = getInput("path", { required: true });
   const config = getInput("config", { required: false });
 
-  info(
-    JSON.stringify({
-      token,
-      path,
-      config,
-    })
-  );
+  const oas = new OASNormalize(path, { enablePaths: true });
+  await oas.load();
+
+  info("OAS Specification loaded. Version: " + (await oas.version()).version);
 
   return;
 }
