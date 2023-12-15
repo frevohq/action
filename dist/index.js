c7983b620f3e3ad272869935e1227b9934658e2e
Ruan Martinelli <martinelliruan@gmail.com>
2023-12-15 10:56:25 +0000

    +

--- dist/index.js
+++ dist/index.js
@@ -18,19 +18,15 @@ const swagger_parser_1 = __importDefault(require("@apidevtools/swagger-parser"))
 const pako_1 = __importDefault(require("pako"));
 const undici_1 = require("undici");
 const getSizeInMB = (data) => {
-    const sizeInBytes = Buffer.byteLength(JSON.stringify(data), "utf8");
+    const sizeInBytes = Buffer.byteLength(JSON.stringify(data), 'utf8');
     return (sizeInBytes / (1024 * 1024)).toFixed(2);
 };
 const debug = (message) => (0, core_1.debug)(`[frevo] ${message}`);
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        let token = (0, core_1.getInput)("frevo_token", { required: true });
-        let path = (0, core_1.getInput)("path", { required: true });
-        let config = (0, core_1.getInput)("config", { required: false });
-        // Uncomment for local testing
-        // let token = "";
-        // let path = "./fixture/2-petstore-ref/openapi.yaml";
-        // let path = "./fixture/3-stripe/openapi.json";
+        let token = (0, core_1.getInput)('frevo_token', { required: true });
+        let path = (0, core_1.getInput)('path', { required: true });
+        let config = (0, core_1.getInput)('config', { required: false });
         debug(`path: ${path}`);
         const spec = yield swagger_parser_1.default.bundle(path, {});
         debug(`total spec size ${getSizeInMB(spec)} MB`);
@@ -40,13 +36,13 @@ function main() {
         const body = pako_1.default.gzip(uint8);
         debug(`compressing done`);
         debug(`uploading`);
-        const response = yield (0, undici_1.fetch)("https://frevo-api-69.localcan.dev/openapi", {
-            method: "POST",
+        const response = yield (0, undici_1.fetch)('https://api.frevo.dev/openapi', {
+            method: 'POST',
             body,
             headers: {
-                "Content-Encoding": "gzip",
-                "Content-Type": "application/json",
-                "Content-Length": body.length.toString(),
+                'Content-Encoding': 'gzip',
+                'Content-Type': 'application/json',
+                'Content-Length': body.length.toString(),
                 Authorization: `Bearer ${token}`,
             },
         });
